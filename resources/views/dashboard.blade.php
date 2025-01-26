fa6e139097f2417bd5ae262022bb6dfcc75ddcb5
Nyi Myat <nyinyimyat611@gmail.com>
2025-01-26 21:36:05 +0630

    dashboard upd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,12 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
         <section class="h-[700px] relative slider-container">
-            <!-- Background Image -->
             <div class="slider-img absolute top-0 left-0 w-full h-full">
                 <img src="{{asset('img/slider.jpg')}}" alt="" class="relative w-full h-full">
             </div>
 
-            <!-- Content Section (Text and Button) -->
             <div class="absolute top-[50%] left-[23%] transform -translate-x-[40%] -translate-y-1/2 bg-black bg-opacity-30 p-5 rounded-md z-10">
                 <h1 class="text-white text-2xl font-bold text-center">Clean Is Not Just a Look; It’s a Lifestyle</h1>
                 <h3 class="text-white text-1.5xl mt-2">Professional cleaning services that leave your home sparkling. Book your cleaning today!</h3>
@@ -19,7 +17,7 @@
                     </button>
                 </form>
             </div>
-            
+
         </section>
     </x-slot>
 </x-app-layout>
